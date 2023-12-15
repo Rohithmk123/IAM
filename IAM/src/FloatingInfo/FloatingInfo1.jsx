87db217a55aae5aa8df9ff461d2c042c420c487b
Rohith M K <rohithmk8055@gmail.com>
2023-12-14 21:14:51 -0800

    commit

--- IAM/src/FloatingInfo/FloatingInfo1.jsx
+++ IAM/src/FloatingInfo/FloatingInfo1.jsx
@@ -33,17 +33,39 @@ function FloatingInfo1() {
     }
   }, [scrollPosition]);
 
-  return (
-    <div id="floatingInfo1" style={{ position: 'absolute', left: position, top: '900px', zIndex: '1', width: '350px', height: '300px', padding: '20px', backgroundColor: 'rgba(240, 240, 240, 0.8)', transition: 'left 0.5s ease-in-out', textAlign: 'center', color: 'black',borderRadius:'50px' }}>
+  return (<Link to="Enterococcus Page">
+    <div id="floatingInfo1" style={{ position: 'absolute', left: position, top: '900px', zIndex: '1', width: '350px', height: '350px', padding: '20px', backgroundColor: 'rgba(240, 240, 240, 0.8)', transition: 'left 0.5s ease-in-out', textAlign: 'center', color: 'black',borderRadius:'50px' }}>
       {/* Add your additional information content here */}
       <div>
         
-        <p style={{ cursor: 'pointer', textDecoration: '',textDecorationColorcolor:'black' }}>
-         <Link to="Enterococcus Page"> 1 Enterococcus faecium.</Link>
-        </p>
+        <p style={{  textDecoration: '',textDecorationColorcolor:'black',textAlign: 'center' }}>
+                 <h5>Enterococcus faecium.</h5> </p>
+                 <img
+            src='/e facum.jpg' // Replace with the actual path to your image
+            alt="Enterococcus Image"
+            style={{ width: '90px', height: '90px', position: 'absolute', top: '70px', left: '10px' }}
+          />
+      
+       <div style={{ fontSize: '15px', paddingTop: '10px' }}>
+      <div style={{ marginLeft: '20%' }}><h7 style={{ fontWeight:'bold' }}>Gram stain</h7> : Gram Positive</div>
+            <div style={{ marginLeft: '3%'}}><h7 style={{ fontWeight:'bold' }}>Shape</h7> :   Spherical</div>
+            <div style={{ marginLeft: '27%' }}> <h7 style={{ fontWeight:'bold' }}>Infection </h7> :  nosocomial
+                   infections such as bacteremia, endocarditis,
+                  and urinary tract and surgical wound infections.
+                      </div>
+                      
+          </div>
+          <div>Notably, in February 2017, the World Health Organization (WHO) designated
+                            vancomycin-resistant E. faecium as a high-priority pathogen due to a significant increase in its
+                           prevalence.</div>
+
+
+            
+     
         
       </div>
     </div>
+  </Link>
   );
 }
 
