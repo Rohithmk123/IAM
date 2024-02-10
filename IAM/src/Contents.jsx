48744204241091e18287d859892055280148794e
Rohith M K <rohithmk8055@gmail.com>
2024-02-10 10:19:30 +0530

    coordinates added

--- IAM/src/Contents.jsx
+++ IAM/src/Contents.jsx
@@ -5,14 +5,21 @@
 
 function Contents() {
   return (
+  <div>
     <div style={{ backgroundImage: 'url("./10background.gif")', backgroundSize: 'cover', minHeight: 'calc(100vh - 60px)', marginTop: '80px', paddingBottom: '500px'  }}>
-    {<div><h1>Indian Anti-Microbial Resistance Mutation Database</h1>
+    <div><h1>Indian Anti-Microbial Resistance Mutation Database</h1>
     <p>In India, we have a legacy of nearly 100 years. With our new purpose, strategy and
       <br/> culture, we will move #AheadTogether continuing our legacy in General Medicines, 
       <br/>our leadership in the Vaccines  self-pay market and introduce innovative offerings 
       <br/>through our Specialty medicines.</p>
     </div>
-    }
+    <div><img
+            src='/1.jpg' // Replace with the actual path to your image
+            alt="Acinetobacter baumannii Image"
+            style={{ width: '150px', height: '150px', position: 'absolute', top: '750px', left: '930px', zIndex: '5' }}
+            /></div>
+    </div>
+    
 
 
 
